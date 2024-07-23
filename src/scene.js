b8c9bd9a8f084b5baf3dae00dacab8da1ca058a4
d0sob <kitzeru@gmail.com>
2024-07-23 11:29:20 +0530

    Ready for Pages!

--- src/scene.js
+++ src/scene.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 class SceneInit {
   constructor() {
@@ -13,14 +14,54 @@ class SceneInit {
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setAnimationLoop(this.render.bind(this));
-    document.body.appendChild(this.renderer.domElement);
+    document
+      .getElementById("threejs-stuff")
+      .appendChild(this.renderer.domElement);
+
+    this.renderer.toneMapping = THREE.NeutralToneMapping;
+
+    // ADDING LIGHTS
+    ////////////////////////////////////////////////////
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 1);
+    this.scene.add(this.ambientLight);
+
+    this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    this.directionalLight.position.set(5, 10, 7.5);
+    this.scene.add(this.directionalLight);
+    /////////////////////////////////////////////////////
+
+    // ADDING MODEL
+    this.loader = new GLTFLoader();
+    this.loader.load(
+      "../nightmare.glb",
+      (gltf) => {
+        this.bbox = new THREE.Box3().setFromObject(gltf.scene);
+        this.center = this.bbox.getCenter(new THREE.Vector3());
+
+        // Correct typo here: 'positon' should be 'position'
+        gltf.scene.position.sub(this.center);
+
+        this.scale = 1;
+        gltf.scene.scale.set(this.scale, this.scale, this.scale);
+
+        // Correct typo here: 'gtlf' should be 'gltf'
+        this.scene.add(gltf.scene);
+      },
+      undefined,
+      (e) => {
+        console.error(e);
+      }
+    );
 
     this.raycaster = new THREE.Raycaster();
     this.mouse = new THREE.Vector2();
 
     window.addEventListener("mousemove", (e) => this.onMouseMove(e), false);
 
-    this.camera.position.z = 5;
+    this.radius = 5;
+    this.speed = 0.01;
+    this.angle = 0;
+    this.target = new THREE.Vector3(0, 0, 0);
 
     window.addEventListener("resize", this.onWindowResize.bind(this), false);
   }
@@ -58,6 +99,12 @@ class SceneInit {
   }
 
   render() {
+    this.angle += this.speed;
+    const x = this.target.x + this.radius * Math.cos(this.angle);
+    const z = this.target.z + this.radius * Math.sin(this.angle);
+    this.camera.position.set(x, this.camera.position.y, z);
+    this.camera.lookAt(this.target);
+
     this.renderer.render(this.scene, this.camera);
   }
 
